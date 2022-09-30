8ffdc3c6248ea04691e681ccbc0f9f3d7a2ae468
lwijena <lawrencevijenathan@rogers.com>
2022-09-30 11:27:21 -0400

    Add express.js server

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-function say(message: string): void {
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+app.listen(port, () => console.log(`listen on http://localhost:${port}/`));
+
+
+/*function say(message: string): void {
     console.log(`I said: ${message}`);
 }
 say("Hello");
+*/
